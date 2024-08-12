cce24d408550fdc31c95a552dc5fe2d8d65ae5b4
2YH02 <yonghuni484@gmail.com>
2024-08-12 16:25:54 +0900

    Feat: 메인 검색 키워드 검색에서 주소 검색으로 변경

--- lib/api/common/get-search-location.ts
+++ lib/api/common/get-search-location.ts
@@ -2,7 +2,7 @@ import { type LocationResponse } from "@/types/kakao-location.type";
 
 const getSearchLoation = async (query: string): Promise<LocationResponse> => {
   const response = await fetch(
-    `https://dapi.kakao.com/v2/local/search/keyword.json?query=${query}&page=1&size=9`,
+    `https://dapi.kakao.com/v2/local/search/address?query=${query}&page=1&size=9`,
     {
       headers: {
         Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAK}`,
