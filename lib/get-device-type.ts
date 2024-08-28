bc6ab4398af38fbf28565fd3ca64930e52e06006
2YH02 <yonghuni484@gmail.com>
2024-08-28 20:26:49 +0900

    Fix: 디바이스 타입 값 수정

--- lib/get-device-type.ts
+++ lib/get-device-type.ts
@@ -1,27 +1,27 @@
 const getDeviceType = (userAgent: string) => {
-  //   if (userAgent.includes("android-mobile-app")) {
-  //     return "android-mobile-app";
-  //   } else if (userAgent.includes("ios-mobile-app")) {
-  //     return "ios-mobile-app";
-  //   } else if (/android/i.test(userAgent)) {
-  //     return "android-mobile-web";
-  //   } else if (/iphone|ipad|ipod/i.test(userAgent)) {
-  //     return "ios-mobile-web";
-  //   } else {
-  //     return "desktop";
-  //   }
-
   if (userAgent.includes("android-mobile-app")) {
     return "android-mobile-app";
   } else if (userAgent.includes("ios-mobile-app")) {
-    return "desktop";
+    return "ios-mobile-app";
   } else if (/android/i.test(userAgent)) {
     return "android-mobile-web";
   } else if (/iphone|ipad|ipod/i.test(userAgent)) {
     return "ios-mobile-web";
   } else {
-    return "ios-mobile-app";
+    return "desktop";
   }
+
+  // if (userAgent.includes("android-mobile-app")) {
+  //   return "android-mobile-app";
+  // } else if (userAgent.includes("ios-mobile-app")) {
+  //   return "desktop";
+  // } else if (/android/i.test(userAgent)) {
+  //   return "android-mobile-web";
+  // } else if (/iphone|ipad|ipod/i.test(userAgent)) {
+  //   return "ios-mobile-web";
+  // } else {
+  //   return "ios-mobile-app";
+  // }
 };
 
 export default getDeviceType;
