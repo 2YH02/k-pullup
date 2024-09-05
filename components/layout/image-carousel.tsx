5343a68b26e252c8aef866159931d1281d373620
2YH02 <yonghuni484@gmail.com>
2024-09-05 14:27:16 +0900

    Feat: 삭제된 위치 최근 추가 이미지에서 제외

--- components/layout/image-carousel.tsx
+++ components/layout/image-carousel.tsx
@@ -4,6 +4,7 @@ import type { newPicturesRes } from "@api/marker/new-pictures";
 import { Carousel, CarouselContent, CarouselItem } from "@common/carousel";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface ImageCarouselProps {
   data: newPicturesRes[];
@@ -21,13 +22,36 @@ const ImageCarousel = ({
   onClick,
 }: ImageCarouselProps) => {
   const router = useRouter();
+
+  const [validData, setValidData] = useState<newPicturesRes[]>([]);
+
+  useEffect(() => {
+    const validateImages = async () => {
+      const validatedData = await Promise.all(
+        data.map(
+          (item) =>
+            new Promise<newPicturesRes | null>((resolve) => {
+              const img = new window.Image();
+              img.src = item.photoURL;
+              img.onload = () => resolve(item);
+              img.onerror = () => resolve(null);
+            })
+        )
+      );
+
+      setValidData(validatedData.filter(Boolean) as newPicturesRes[]);
+    };
+
+    validateImages();
+  }, [data]);
+
   const carouselSize =
     size === "sm" ? "w-12 h-12" : size === "md" ? "w-24 h-24" : "w-32 h-32";
 
   return (
     <Carousel opts={{ dragFree: true }}>
       <CarouselContent className={`-ml-1 gap-3 ${carouselSize} p-1`}>
-        {data.map((item) => (
+        {validData.map((item) => (
           <CarouselItem key={item.markerId} className="p-0">
             <button
               className="w-full h-full overflow-hidden rounded-lg shadow-md"
