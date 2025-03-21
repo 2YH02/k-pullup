40ebeb453e5a4c9cfe760140ea76f9e9a45ef306
2YH02 <yonghuni484@gmail.com>
2025-03-21 23:50:50 +0900

    Feat: 서비스 리다이랙트 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,33 @@ const nextConfig = {
     domains: ["chulbong-kr.s3.amazonaws.com", "t1.daumcdn.net"],
     unoptimized: true,
   },
+
+  async redirects() {
+    return [
+      {
+        source: "/mypage",
+        has: [
+          {
+            type: "host",
+            value: "www.k-pullup.com",
+          },
+        ],
+        destination: "https://m.k-pullup.com/me",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.k-pullup.com",
+          },
+        ],
+        destination: "https://m.k-pullup.com",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
