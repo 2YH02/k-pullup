e350ac3aece09a88d14f3010a9720bf172b20b96
Yonghun Yi <yonghun.yi@cj.net>
2026-02-26 09:05:32 +0900

    ci: Playwright 테스트 파이프라인 성능 개선

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   testDir: "./tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 2 : undefined,
   reporter: "html",
   use: {
     baseURL: "https://local.k-pullup.com:5173",
